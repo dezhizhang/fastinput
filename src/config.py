35b54207c01d2599c8fed33d62ff29ffabcf1eb9
dezhizhang <1541609448@qq.com>
2026-03-27 05:52:21 +0800

    feat:数据的预处理

---
+++ src/config.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+
+
+ROOT_DIR = Path(__file__).parent.parent
+
+
+# 数据文件目录
+RAW_DATA_DIR = ROOT_DIR / 'data' / 'raw'
+
+# 数据预处理目录
+PROCESSED_DATA_DIR = ROOT_DIR / 'data' / 'processed'
+
+# 日志目录
+LOGS_DIR = ROOT_DIR / 'logs'
+
+
+
+
+
+
--- src/process.py
+++ src/process.py
@@ -1,17 +1,18 @@
 import pandas as pd
+import config
 
 
 
 def process():
     """数理预处理"""
 
+    # 文件所在目录
+    data_dir = config.RAW_DATA_DIR / 'synthesized_.jsonl'
+
     # 读取数据文件
-    df = pd.read_json('../data/raw/synthesized_.jsonl',lines=True,orient='records')
+    df = pd.read_json(data_dir, lines=True, orient='records')
 
     print(df.head())
-
-
-
 
 
 if __name__ == '__main__':
