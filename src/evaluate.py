fc0f35eaafd03237f17e82dc1cad12d548578837
dezhizhang <1541609448@qq.com>
2026-03-29 17:23:47 +0800

    feat:模型评估

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,24 +1,43 @@
 import torch
 import config
-from  model import FastInputModel
+from model import FastInputModel
 from src.dataset import get_dataloader
+from predict import predict_batch
 
 
 def evaluate(model, test_dataloader, device):
-    """模型评估逻辑"""
-    model.eval()
+    """
+    模型评估
+    :param model:
+    :param test_dataloader:
+    :param device:
+    :return:
+    """
 
-    with torch.no_grad():
-
-        for inputs,targets in test_dataloader:
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-
-
-            output = model(inputs)
+    top1_acc_count = 0
+    top5_acc_count = 0
+    total_count = 0
 
 
 
+    for inputs, targets in test_dataloader:
+        inputs = inputs.to(device)
+
+        targets = targets.to(device)
+
+
+        top5_indexes_list = predict_batch(model, inputs)
+
+        for target,top5_indexes in zip(targets, top5_indexes_list):
+            top1_acc_count += 1
+            if target == top5_indexes[0]:
+                top1_acc_count += 1
+
+            if target in top5_indexes:
+                top5_acc_count += 1
+
+
+    return top1_acc_count / total_count, top5_acc_count / total_count
 
 
 
@@ -30,20 +49,27 @@ def run_evaluate():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # 2. 加载词表
-    with open(config.MODELS_DIR / 'vocab.txt','r',encoding='utf-8') as f:
+    with open(config.MODELS_DIR / 'vocab.txt', 'r', encoding='utf-8') as f:
         vocab_list = [line.strip() for line in f.readlines()]
 
     # 3. 加载模型
     model = FastInputModel(vocab_size=len(vocab_list)).to(device)
     model.load_state_dict(torch.load(config.MODELS_DIR / 'best.pth'))
 
-
     # 4. 获取数据集
     test_dataloader = get_dataloader()
 
-
     # 5. 评估模型
-    top1_acc,top5_acc = evaluate(model, test_dataloader, device)
+    top1_acc, top5_acc = evaluate(model, test_dataloader, device)
+
+
+    print("评估结果")
+    print(f"top1_acc:{top1_acc}")
+    print(f"top5_acc:{top5_acc}")
+
+
+if __name__ == "__main__":
+    run_evaluate()
 
 
 
