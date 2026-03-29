0dbc4a9f5697ace792bfd543ce8a285a80513901
dezhizhang <1541609448@qq.com>
2026-03-29 15:51:17 +0800

    feat:模型预测

--- src/predict.py
+++ src/predict.py
@@ -3,23 +3,8 @@ import torch
 import config
 from model import FastInputModel
 
-def predict(text):
+def predict(text,model,word2index,index2word,device):
     """模型预测"""
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # 2. 词表
-    with open(config.MODELS_DIR / 'vocab.txt', 'r',encoding='utf-8') as f:
-        vocab_list = [line.strip() for line in f.readlines()]
-
-    word2index = {word:index for index, word in enumerate(vocab_list)}
-    index2word = {index:word for index, word in enumerate(vocab_list)}
-
-
-
-
-    # 3. 模型
-    model = FastInputModel(vocab_size=len(vocab_list)).to(device)
-    model.load_state_dict(torch.load(config.MODELS_DIR / 'best.pth'))
 
     # 4. 处理输入
     tokens = jieba.lcut(text)
@@ -47,11 +32,42 @@ def predict(text):
 
 
 def run_predict():
+
+    # 准备资源
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # 2. 词表
+    with open(config.MODELS_DIR / 'vocab.txt', 'r',encoding='utf-8') as f:
+        vocab_list = [line.strip() for line in f.readlines()]
+
+    word2index = {word:index for index, word in enumerate(vocab_list)}
+    index2word = {index:word for index, word in enumerate(vocab_list)}
+
+    # 3. 模型
+    model = FastInputModel(vocab_size=len(vocab_list)).to(device)
+    model.load_state_dict(torch.load(config.MODELS_DIR / 'best.pth'))
+
+
     while True:
         user_input = input(">")
+
+        if user_input in ['q','quit']:
+            print("欢迎下次再来")
+            break
+        if user_input.strip() == '':
+            print("请输入内容")
+            continue
+
+        token5 = predict(user_input,model,word2index,index2word,device)
+
+
+        print(token5)
+
+
 
 
 
 if __name__ == "__main__":
-    top_tokens = predict("我们团队")
+    run_predict()
+
 
